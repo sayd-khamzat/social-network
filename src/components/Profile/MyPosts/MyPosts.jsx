7b762424073a3078ccf7df928e6a7ac4ea5b0a56
sayd-khamzat <111534427+sayd-khamzat@users.noreply.github.com>
2023-03-04 23:51:28 +0300

    Using React.memo

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import Post from "./Post/Post";
 import AddPostForm from "./AddPostForm/AddPostForm";
 
-const MyPosts = (props) => {
+const MyPosts = React.memo((props) => {//memo добавил, но разницы нет.
+    // Мемо должен сравнивать пред. и след. пропсы, для того, чтобы небыло лишних перерисовок.
+    // Перерисовок и без мемо нет.
 
     const profilePage = props.profilePage;
     const postsElements = profilePage.postsData.map(post => <Post key={post.id} message={post.message} likesCount={post.likesCount}/>);
@@ -22,6 +24,6 @@ const MyPosts = (props) => {
             </div>
         </div>
     );
-}
+})
 
 export default MyPosts;
